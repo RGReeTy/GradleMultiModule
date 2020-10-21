7f0985a3a1ef433273c9b0612d260f8bec53ce45
RGReeTy <const.hsky@gmail.com>
2020-10-21 16:16:29 +0300

    refactoring method (using Stream.api)

--- core/src/main/java/by/mjc/util/Util.java
+++ core/src/main/java/by/mjc/util/Util.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Util {
 
     public boolean isAllPositiveNumbers(String... strings) {
-        StringUtils checkerNumbers = new StringUtils();
+        StringUtils numbersChecker = new StringUtils();
 
-        return Arrays.stream(strings).allMatch(checkerNumbers::isPositiveNumber);
+        return Arrays.stream(strings).allMatch(numbersChecker::isPositiveNumber);
     }
 }
